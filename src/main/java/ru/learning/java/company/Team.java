9a393df2bbf3cc22bf2a8d92505b65d0f6626068
Александр Самаркин <aysamarkin@sberbank.ru>
2025-12-02 20:49:04 +0300

    added generic

---
+++ src/main/java/ru/learning/java/company/Team.java
@@ -0,0 +1,30 @@
+package ru.learning.java.company;
+
+import ru.learning.java.employees.Employee;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Team<T extends Employee> {
+  private final String teamName;
+  private final List<T> participants = new ArrayList<>();
+
+  public Team(String teamName) {
+    this.teamName = teamName;
+  }
+
+  public void addParticipant(T participant) {
+    participants.add(participant);
+    System.out.println("В команду " + teamName + " добавлен сотрудник: " + participant.getName());
+  }
+
+  public void playTeam() {
+    System.out.println("Команда " + teamName + " начинает работу:");
+    for (T participant : participants) {
+      participant.work();
+    }
+  }
+
+  public List<T> getParticipants() {
+    return participants;
+  }
+}
