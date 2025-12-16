1e56b5add6361824294c3aee4344de496b40bdf3
Александр Самаркин <aysamarkin@sberbank.ru>
2025-12-16 19:04:00 +0300

    delete unused import

--- src/main/java/ru/learning/java/service/EmployeeEventConsumer.java
+++ src/main/java/ru/learning/java/service/EmployeeEventConsumer.java
@@ -6,8 +6,6 @@ import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import ru.learning.java.model.EmployeeEvent;
 
-import java.sql.SQLOutput;
-
 public class EmployeeEventConsumer {
 
     @KafkaListener(topics = "test-project-input", groupId = "my-group")
